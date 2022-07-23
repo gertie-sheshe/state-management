9ba645bb582e936aa7547c772c7b8f2e4f3fe710
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-07-23 12:36:32 +0300

    (test) add todo unit tests

--- src/components/Todo/Todo.test.js
+++ src/components/Todo/Todo.test.js
@@ -1,9 +1,20 @@
 import { render } from "../../tests/utils";
+import { screen } from "@testing-library/react";
 import Todo from "./Todo";
 
 describe("Todo", () => {
   it("matches snapshot", () => {
-    const { asFragment } = render(<Todo todo={{ id: 1, title: "test" }} />);
+    const { asFragment } = render(
+      <Todo todo={{ id: 1, title: "test", completed: false }} />
+    );
     expect(asFragment()).toMatchSnapshot();
+  });
+  it("shows checkbox with unchecked state", () => {
+    render(<Todo todo={{ id: 1, title: "test", completed: false }} />);
+    expect(screen.getByRole("checkbox").checked).toBe(false);
+  });
+  it("shows checkbox with checked state", () => {
+    render(<Todo todo={{ id: 1, title: "test", completed: true }} />);
+    expect(screen.getByRole("checkbox").checked).toBe(true);
   });
 });
