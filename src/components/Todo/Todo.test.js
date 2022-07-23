0f7a87f6fcdb35bbb991c81cab3f0c4b0ac49522
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-07-23 12:03:53 +0300

    (test) add TodoForm unit tests

--- src/components/Todo/Todo.test.js
+++ src/components/Todo/Todo.test.js
@@ -3,7 +3,7 @@ import Todo from "./Todo";
 
 describe("Todo", () => {
   it("matches snapshot", () => {
-    const { asFragment } = render(<Todo todo={{ title: "test" }} />);
+    const { asFragment } = render(<Todo todo={{ id: 1, title: "test" }} />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- src/components/TodoForm/TodoForm.test.js
+++ src/components/TodoForm/TodoForm.test.js
@@ -1,9 +1,35 @@
 import { render } from "../../tests/utils";
+import { screen } from "@testing-library/react";
 import TodoForm from "./TodoForm";
 
 describe("TodoForm", () => {
   it("matches snapshot", () => {
     const { asFragment } = render(<TodoForm />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("shows input field with an empty value", () => {
+    render(<TodoForm />);
+    expect(screen.getByRole("textbox").value).toBe("");
+  });
+
+  it("shows disabled button when userId is not provided", () => {
+    render(<TodoForm />);
+    expect(screen.getByRole("button").disabled).toBe(true);
+  });
+
+  it("shows enabled button when userId is provided", () => {
+    render(<TodoForm userId={1} />);
+    expect(screen.getByRole("button").disabled).toBe(false);
+  });
+
+  it("shows disabled input field when userId is not provided", () => {
+    render(<TodoForm />);
+    expect(screen.getByRole("textbox").disabled).toBe(true);
+  });
+
+  it("shows enabled input field when userId is provided", () => {
+    render(<TodoForm userId={1} />);
+    expect(screen.getByRole("textbox").disabled).toBe(false);
   });
 });
