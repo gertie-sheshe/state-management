950ded1091b7fa36b9a641c44b2579cfa8f75e36
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-07-23 14:34:31 +0300

    (chore) add msw congifuration

--- src/components/Users/Users.test.js
+++ src/components/Users/Users.test.js
@@ -1,9 +1,20 @@
 import { render } from "../../tests/utils";
 import Users from "./Users";
+import { screen } from "@testing-library/react";
+import { worker } from "../../tests/mocks/server";
 
 describe("Users", () => {
+  beforeAll(() => worker.listen());
+  afterEach(() => worker.resetHandlers());
+  afterAll(() => worker.close());
   test("Matches snapshot", () => {
     const { asFragment } = render(<Users />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("Shows loading state", async () => {
+    render(<Users />);
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+    // expect(container.querySelector(".loading")).toBeInTheDocument();
   });
 });
---
+++ src/tests/mocks/handlers.js
@@ -0,0 +1,76 @@
+import { rest } from "msw";
+const API_URL = "http://localhost:3000";
+
+const fetchUsers = rest.get(`${API_URL}/users`, (req, res, ctx) => {
+  return res(
+    ctx.json([
+      {
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+      },
+      {
+        id: 2,
+        name: "Ervin Howell",
+        username: "Antonette",
+        email: "Shanna@melissa.tv",
+      },
+      {
+        id: 3,
+        name: "Clementine Bauch",
+        username: "Samantha",
+        email: "Nathan@yesenia.net",
+      },
+      {
+        id: 4,
+        name: "Patricia Lebsack",
+        username: "Karianne",
+        email: "Julianne.OConner@kory.org",
+      },
+    ])
+  );
+});
+
+const fetchUsersTodos = rest.get(
+  `${API_URL}/users/:id/todos`,
+  (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: "delectus aut autem",
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 5,
+          title:
+            "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 9,
+          title: "molestiae perspiciatis ipsa",
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 13,
+          title: "et doloremque nulla",
+          completed: false,
+        },
+        {
+          title: "home alone",
+          completed: false,
+          userId: "1",
+          id: 14,
+        },
+      ])
+    );
+  }
+);
+
+export const handlers = [fetchUsers, fetchUsersTodos];
